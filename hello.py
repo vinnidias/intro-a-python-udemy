ee09d0af20cc6d28c4c3e70600cfab76a8e28cc4
vinnidias <vinicius.3991@gmail.com>
2020-12-18 14:28:06 -0300

    print, comentários e váriáveis

---
+++ hello.py
@@ -0,0 +1,28 @@
+print("Olá mundo")
+print("Outra mesagem")
+
+# hashtag para comentar
+
+#operações matemáticas
+
+print(2 + 8)
+
+print(10 * 3)
+
+print(10 % 3)
+
+# declaração de variáveis
+
+minha_variavel = "Eu sou uma string"
+
+print(minha_variavel)
+
+var_inteiro = 10
+
+var_float = 10.45
+
+var_bool = True
+
+var_bool_false = False
+
+
