3e22c1b20b8605571daf88eefbd3d9ceabfac869
vinnidias <vinicius.3991@gmail.com>
2020-12-21 10:55:01 -0300

    estruturas de repetição

---
+++ estruturas_de_repeticao.py
@@ -0,0 +1,25 @@
+# while
+
+x = 1
+
+while x < 10:
+    print(x)
+    x +=1 # x = x+1
+
+
+#for 
+
+lista1 = [1,2,3,4,5]
+lista2 = ['ola', 'mundo', '!']
+lista3 = ['string', True, 9.99, 10]
+
+for i in lista3:
+    print(i)
+
+# range
+
+for j in range(10,20):
+    print(j)
+
+for c in range(10,20,2):
+    print(c)
